59c8a48964763cc3bc17db3d08766e9667013ca3
kalasu-1999 <git@github.com:kalasu-1999/Java.git>
2020-05-19 13:18:03 +0800

    demo01

---
+++ dataProcessing/findLines.py
@@ -0,0 +1,21 @@
+import numpy
+
+
+# cPosition传入错误文件所在位置,如:/home/target.c
+# dirPosition:/home
+def findLines(cPosition, dirPosition):
+    file = open(cPosition)
+    lines = file.readlines()
+    countList = []
+    for i in range(lines.__len__()):
+        if not (lines[i].startswith('#') or lines[i].startswith('//') or lines[i] == '' or lines[i] == '\n'):
+            if not ('{' in lines[i] or '}' in lines[i]):
+                countList.append(i)
+    numpy.save(dirPosition + "/numpyDataDir/findLines.npy", countList)
+    for item in countList:
+        print(item)
+
+
+if __name__ == '__main__':
+    findLines("/home/kalasu/PycharmProjects/tot_info/target.c",
+              "/home/kalasu/PycharmProjects/tot_info")
